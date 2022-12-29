92fde9449d29ababda711d70dcf2446571f2800a
Onur Alp Bicer <onuralpbicer@gmail.com>
2022-12-29 16:37:35 +1100

    Added price and reviewer name to DB

--- server/schema/server.sql
+++ server/schema/server.sql
@@ -18,14 +18,17 @@ CREATE TABLE IF NOT EXISTS products (
     subcategory_id uuid NOT NULL,
     description TEXT NOT NULL,
     stock INTEGER DEFAULT 0,
+    price INTEGER NOT NULL,
     primary key(id),
     constraint fk_products foreign key (subcategory_id) references subcategories(id),
-    constraint stock_check check (stock >= 0)
+    constraint stock_check check (stock >= 0),
+    constraint price_check check (price >= 0)
 );
 
 CREATE TABLE IF NOT EXISTS reviews (
     product_id uuid NOT NULL,
     rating INTEGER NOT NULL,
+    name text NOT NULl,
     review TEXT,
     constraint fk_reviews foreign key (product_id) references products(id),
     constraint rating_check check (rating > 0 AND rating < 6)
